0753fa4ab7391060e557a46fe6b8138c6bc9ff0e
ChandrakantPal <sanjay161097@gmail.com>
2021-08-09 21:29:36 +0530

    small cards used

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import SmallCard from '../components/SmallCard'
+import { City } from '../types'
 
-export default function Home() {
+const Home = ({ expoloreData, cardData }) => {
   return (
     <div className="">
       <Head>
@@ -13,6 +15,44 @@ export default function Home() {
       <Header />
       {/* Banner */}
       <Banner />
+
+      <main className="px-8 mx-auto max-w-7xl sm:px-16">
+        <section className="pt-6">
+          <h2 className="pb-5 text-4xl font-semibold">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {expoloreData?.map(({ img, location, distance }: City) => (
+              <SmallCard
+                img={img}
+                distance={distance}
+                location={location}
+                key={img}
+              />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h2 className="py-8 text-4xl font-semibold">Live Anywhere</h2>
+        </section>
+      </main>
     </div>
   )
+}
+
+export default Home
+
+export async function getStaticProps() {
+  const expoloreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  )
+
+  const cardData = await fetch('https://links.papareact.com/zp1').then((res) =>
+    res.json()
+  )
+
+  return {
+    props: {
+      expoloreData,
+      cardData,
+    },
+  }
 }
