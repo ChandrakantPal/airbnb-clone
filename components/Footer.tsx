170cd07b4c2c5a1136bfbf7c6f0af8c11e9ad2a6
ChandrakantPal <sanjay161097@gmail.com>
2021-08-10 16:25:30 +0530

    footer cmponent

---
+++ components/Footer.tsx
@@ -0,0 +1,39 @@
+const Footer = () => {
+  return (
+    <footer className="grid grid-cols-1 px-32 text-gray-600 bg-gray-100 md:grid-cols-4 gap-y-10 py-14">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accssibility</p>
+        <p>This is not a real site</p>
+        <p>Its a pretty awesome clone</p>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Lorem ipsum </p>
+        <p>Lorem ipsum dolor</p>
+        <p>Lorem ipsum </p>
+        <p>Join Now</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Center</p>
+        <p>Trust & Safety</p>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum Plus</p>
+        <p>Lorem ipsum Luxe</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
