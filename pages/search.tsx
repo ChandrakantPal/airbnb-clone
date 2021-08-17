257ac20d7b3f50fe234859472ab1792e4385d13a
ChandrakantPal <sanjay161097@gmail.com>
2021-08-17 22:35:08 +0530

    main section

--- pages/search.tsx
+++ pages/search.tsx
@@ -5,6 +5,15 @@ const Search = () => {
   return (
     <div>
       <Header />
+      <main className="flex">
+        <section>
+          <p className="text-xs">300</p>
+          <h1 className="mt-2 mb-6 text-3xl font-semibold">Stays in Mars</h1>
+          <div className="hidden">
+            <p>Cancellation Flexibility</p>
+          </div>
+        </section>
+      </main>
       <Footer />
     </div>
   )
