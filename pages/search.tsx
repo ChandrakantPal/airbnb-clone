c1a7fb832b8a422201aa7b727de21852831ccd6a
ChandrakantPal <sanjay161097@gmail.com>
2021-08-17 22:28:40 +0530

    footer added

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,9 +1,11 @@
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 
 const Search = () => {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   )
 }
