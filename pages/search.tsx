62513ef58ec23a411ef3a366994a6504e24cc5e0
ChandrakantPal <sanjay161097@gmail.com>
2021-08-20 23:58:31 +0530

    search query used

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,14 +1,26 @@
+import { format } from 'date-fns'
+import { useRouter } from 'next/dist/client/router'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
 const Search = () => {
+  const router = useRouter()
+  const { location, startDate, endDate, noOfGuests } = router.query
+
+  const formattedStartDate = format(new Date(startDate as string), 'dd MMMM yy')
+  const formattedEndDate = format(new Date(endDate as string), 'dd MMMM yy')
+  const range = `${formattedStartDate} - ${formattedEndDate}`
   return (
     <div>
       <Header />
       <main className="flex">
         <section className="flex-grow px-6 pt-14">
-          <p className="text-xs">300</p>
-          <h1 className="mt-2 mb-6 text-3xl font-semibold">Stays in Mars</h1>
+          <p className="text-xs">
+            300+ Stays - {range} - for {noOfGuests} guests
+          </p>
+          <h1 className="mt-2 mb-6 text-3xl font-semibold">
+            Stays in {location}
+          </h1>
           <div className="hidden mb-5 space-x-3 text-gray-800 lg:inline-flex whitespace-nowrap">
             <p className="button">Cancellation Flexibility</p>
             <p className="button">Type of Place</p>
