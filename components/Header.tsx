e4e1e4fa82ce6a9437ab017cd90a997ec15e520d
ChandrakantPal <sanjay161097@gmail.com>
2021-08-21 00:02:24 +0530

    placeholder prop

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,7 +16,7 @@ import 'react-date-range/dist/styles.css' // main style file
 import 'react-date-range/dist/theme/default.css' // theme css file
 import { useRouter } from 'next/dist/client/router'
 
-const Header = () => {
+const Header = ({ placeholder }) => {
   const [searchInput, setSearchInput] = useState('')
   const [startDate, setStartDate] = useState(new Date())
   const [endDate, setEndDate] = useState(new Date())
