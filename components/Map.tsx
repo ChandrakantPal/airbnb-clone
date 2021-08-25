14552597ef6422c36f311ca89cd37d9e73cbd0a7
ChandrakantPal <sanjay161097@gmail.com>
2021-08-25 22:04:06 +0530

    react map gl used

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,5 +1,22 @@
+import { useState } from 'react'
+import ReactMapGL from 'react-map-gl'
+
 const Map = () => {
-  return <div></div>
+  const [viewport, setViewport] = useState({
+    width: '100%',
+    height: '100%',
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  })
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/chandrakantpal/cksqbkcva10i218o5gtf79drz"
+      mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_KEY}
+      {...viewport}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    ></ReactMapGL>
+  )
 }
 
 export default Map
